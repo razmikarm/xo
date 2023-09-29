cc9123bb8981d5bce0bb5ea3b8f862a11c0586be
Only lessons <ruben98899850.r@mail.com>
2023-09-29 21:10:29 +0400

    changed the last 'for' in the function is_game_over

--- engine.py
+++ engine.py
@@ -15,17 +15,13 @@ def add(grid, value, position):
 def is_game_over(grid):
     if (grid[0][0] == grid[1][1] == grid[2][2] or grid[0][2] == grid[1][1] == grid[2][0]) and grid[1][1] is not None:
         return True
-    for collumn in zip(*grid):
-        if collumn[0] is not None and len(set(row)) == 1:
+    for column in zip(*grid):
+        if column[0] is not None and len(set(row)) == 1:
             return True
     for row in grid:
         if row[0] is not None and len(set(row)) == 1:
             return True
-    flag = True
     for i in range(len(grid)):
         if None in grid[i]:
-            flag = False
-    return flag
-
-    
-            
+            return False
+    return True
