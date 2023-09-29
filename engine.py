7edfb0f2ce9dd01df97d3cbba94827218a133d5b
Saqo <145964484+Saqo858@users.noreply.github.com>
2023-09-29 21:12:41 +0400

    Check if game is over (#8)
    * Added function is_game_over
    
    * Added function is_game_over final version
    
    * changed the second 'if' in function is_game_over
    
    * function is_game_over improved
    
    * function is_game_over improved finally
    
    * changed the last 'for' in the function is_game_over
    
    ---------
    
    Co-authored-by: Only lessons <ruben98899850.r@mail.com>

--- engine.py
+++ engine.py
@@ -12,3 +12,16 @@ def add(grid, value, position):
         raise Exception('The field is not empty')
     grid[line_idx][el_idx] = value
 
+def is_game_over(grid):
+    if (grid[0][0] == grid[1][1] == grid[2][2] or grid[0][2] == grid[1][1] == grid[2][0]) and grid[1][1] is not None:
+        return True
+    for column in zip(*grid):
+        if column[0] is not None and len(set(row)) == 1:
+            return True
+    for row in grid:
+        if row[0] is not None and len(set(row)) == 1:
+            return True
+    for i in range(len(grid)):
+        if None in grid[i]:
+            return False
+    return True
