a7975e5ac6b0027893f58a5a3d5d83d784cdda14
Razmik Poghosyan <54362304+razmikarm@users.noreply.github.com>
2023-09-30 20:32:10 +0400

    Combined all functionalities in main.py (#14)

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+from prompt import get_value, get_position
+from engine import add, is_game_over
+from state import show
+
+
+BOARD_SIZE = 3
+grid = [ [None] * BOARD_SIZE for _ in range(BOARD_SIZE)]
+
 print("Hello!")
 print("Welcome to XO game!!!")
+USER_VALUES = {'user1': get_value()}
+USER_VALUES['user2'] = 1 - USER_VALUES['user1']
+turn = 'user1' if USER_VALUES['user1'] == 1 else 'user2'
 
-grid = [
-    [None, None, None],
-    [None, None, None],
-    [None, None, None],
-]
+show(grid)
+while True:
+    print(turn.upper(), 'turn')
+    pos = get_position(grid)
+    add(grid, USER_VALUES[turn], pos)
+    show(grid)
+    status = is_game_over(grid)
+    if status:
+        print(turn.upper(), 'won')
+        break
+    turn = 'user1' if turn == 'user2' else 'user2'
